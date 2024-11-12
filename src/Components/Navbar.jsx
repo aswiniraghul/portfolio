35f15835833aad8106a8b2c45307f706bd202194
Aswini Raghul <aswiniraghul@gmail.com>
2024-11-12 16:11:20 -0600

    run check

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="mb-2 flex items-center justify-between py-6">
+    <div className= "flex felx-shrink-0 items-center"> 
+        
+    </div>
+    </nav>
+  );
+};
+
+export default Navbar;
